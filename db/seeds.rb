d7e81ef5d5274915d939f9c8a856ff75ac2d7f78
Lexi <mlexifarris@gmail.com>
2023-11-19 13:59:57 -0600

    added model and seeded DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Pic.create!(name:"basenji", url:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Trindle_basenji.jpg/165px-Trindle_basenji.jpg", description:"black and white pupper on the mountain")
+
+Pic.create!(name:"beagle", url:"https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-1294547052-e1692064878477.jpg?w=1024", description:"puppy with brown and white face and huge ears")
+
+Pic.create!(name:"Doberman Pinscher", url:"https://www.thesprucepets.com/thmb/IB93JgKsl4vx0GVy8awg8pBLyeI=/2927x0/filters:no_upscale():strip_icc()/shorthaired-dog-breeds-5090152-05-45d14c5598fd4416aab9e5dd7cc6b175.jpg", description:"good girl with black and brown chest and log tail")
+
+Pic.create!(name:"blue healer", url:"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQNkl3wEFon4-ViCBKAmn5Ej58E84fOEBaS-gu_mPKCJSooGwTM", description:"good boy with gray/ blue fur and brown face")
